16d5aa6827059a5799e1173de40f5d14f09a6d90
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2021-12-17 20:20:19 +1100

    Fixed Header CSS and added logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import Link from 'next/link'
+
 export const Header = () => {
     return (
         <header>
             <div className="nav-container">
                 <div className="nav-wrapper">
-                    <div className="logo-container">
 
+                    <div className="link-container">
+                        <Link href="/"><a>Home</a></Link>
+                        <Link href="/characters"><a>Characters</a></Link>
+                        <Link href="/planets"><a>Planets</a></Link>
                     </div>
+
+                    <div className="logo-container">
+                        <img src="./starwars-logo.png" alt="starwars-logo" class="logo"/>
+                    </div>
+
                     <div className="nav-search-container">
                         <form className="nav-search" method='GET'>
                             <input className="nav-search-input" type="text" placeholder='Search Star Wars' />

9e40bd452c10cc1da567da4e64e518cefc386440
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2021-12-17 20:20:31 +1100

    Added Link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { Header } from '../components/Header'
-import { HomePage } from './HomePage'
 
 export default function Home() {
   return (
@@ -12,8 +11,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* <Header /> */}
-      <HomePage />
     </div>
   )
 }
---
+++ pages/planets.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const planets = () => {
+    return (
+        <div>
+            Planets
+        </div>
+    )
+}
+
+export default planets

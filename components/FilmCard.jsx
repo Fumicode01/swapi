bbb8631694dd3780a016f8dc6ad96df168db28c5
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2021-12-21 16:16:59 +1100

    Added Favorite functions

--- components/FilmCard.jsx
+++ components/FilmCard.jsx
@@ -2,18 +2,27 @@ import Link from 'next/link'
 import React from 'react'
 
 export const FilmCard = (props) => {
-    const id = props.id
 
+    console.log(props.films)
     return (
-        <Link href={'/films/' + id} className='film-card-wrapper'>
-            <a>
-                <h2 className='film-title'>{props.title}</h2>
-                <h3>Director:{props.director}</h3>
-                <p className="film-opening_crawl">{props.opening_crawl}</p>
-                <p>Release Date: {props.release_date}</p>
-            </a>
-           
-            
-        </Link>
+        <div className="films-container">
+            {props.films.map(film => (
+                <div key={film.uid}>
+                    <Link href={'/films/' + film.uid} className='film-card-wrapper'>
+                        <a>
+                            <h2 className='film-title'>{film.properties.title}</h2>
+                            <h3>Director:{film.properties.director}</h3>
+                            <p className="film-opening_crawl">{film.properties.opening_crawl}</p>
+                            <p>Release Date: {film.properties.release_date}</p>
+                        </a>
+                    
+                    </Link>
+                    <button onClick={()=>props.onFavsList(film)
+                     ? props.removeFavs(film)
+                    : props.addFavs(film)
+                    }>Favs</button>
+                </div>
+            ))}
+        </div>
     )
 }
--- components/List.jsx
+++ components/List.jsx
@@ -1,9 +1,12 @@
+import { faFilm } from '@fortawesome/free-solid-svg-icons';
 import React, {useEffect, useState} from 'react'
 import { FilmCard } from './FilmCard';
 
 const List = () => {
     
     const [films, setFilms] = useState([]);
+    const [favs, setFavs] = useState([]);
+
 
     useEffect(() => {
         async function fetchFilms(){
@@ -15,24 +18,92 @@ const List = () => {
     }, [])
     console.log(films)
 
-    function addFavourite(film){
-        localStorage.setItem('favourite', film)
-    };
+    useEffect(() => {
+        console.log(favs);
+      }, [favs]);
+
+      useEffect(() => {
+          const favoritefilms = JSON.parse(localStorage.getItem('favoriteFilms'));
+          setFavs(favoritefilms)
+      },[]);
+
+
+    function addFavs(film){
+        const newFavsList = [...favs, film]
+        setFavs(newFavsList)
+        saveToLocalStorage(newFavsList)
+    }
+
+    function removeFavs(film){
+        const newFavsList = favs.filter((fav) => {return fav.uid !== film.uid})
+        setFavs(newFavsList)
+        saveToLocalStorage(newFavsList)
+    }
+
+    function saveToLocalStorage(films){
+        localStorage.setItem('favoriteFilms', JSON.stringify(films))
+    }
+
+    function onFavsList(film){
+        if (favs.filter(item => item.uid === film.uid).length > 0){
+            return true
+        }
+        return false
+    }
 
     return (
-        <div className="films-container">
-            {films.map(film => (
-                <FilmCard  
-                    key={film.uid}
-                    id={film.uid}
-                    title={film.properties.title}
-                    characters={film.properties.characters}
-                    director={film.properties.director}
-                    opening_crawl={film.properties.opening_crawl}
-                    release_date={film.properties.release_date}
-                    />
-            ))}
-        </div>
+        <>
+                {/* {films.map(film => (
+                    <FilmCard  
+                        key={film.uid}
+                        id={film.uid}
+                        title={film.properties.title}
+                        characters={film.properties.characters}
+                        director={film.properties.director}
+                        opening_crawl={film.properties.opening_crawl}
+                        release_date={film.properties.release_date}
+                        addFavs={addFavs}
+                        removeFavs={removeFavs}
+                        // favs={favs}
+                        />
+                ))} */}
+                <FilmCard 
+                    films={films}
+                    addFavs={addFavs}
+                    removeFavs={removeFavs}
+                    onFavsList={onFavsList}
+                />
+                <div>Favs List</div>
+                <FilmCard 
+                    films={favs}
+                    addFavs={addFavs}
+                    removeFavs={removeFavs}
+                />
+            {/* <div>
+            <h1>Initial list</h1>
+                <ul>
+                    {films.map((item, i) => (
+                    <li key={i}>
+                        {item.name}{" "}
+                        <button
+                        onClick={() => {
+                            addFavs(item);
+                        }}
+                        >
+                        {item.favorite === true ? "Remove" : "Add"}
+                        </button>
+                    </li>
+                    ))}
+                </ul>
+
+                <h1>Favorite list</h1>
+                <ul>
+                    {favs.map(item =>
+                    item.favorite === true ? <li key={item.id}>{item.name}</li> : null
+                    )}
+                </ul>
+            </div> */}
+        </>
     )
 }
 
